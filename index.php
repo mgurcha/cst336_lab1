0fd366c4710b1be683fd9157b58a4dff019c5a1e
Manjit Gurcha <mgurcha@csumb.edu>
2018-02-01 01:26:30 +0000

    add all lab 1 files

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html>
+<!--
+
+Personal Portfolio website
+(comments can span multiple lines)
+
+-->
+
+<!-- This is the head -->
+<!-- All styles and javascript go inside the head -->
+    <head>
+       <meta char="utf-8"/>
+       <title>Manjit Gurcha: Personal Website</title>
+       <link href="css/styles.css" rel="stylesheet" type="text/css"/>
+       <link href="https://fonts.googleapis.com/css?family=Pacifico" rel="stylesheet">
+    </head>
+<!-- closing head -->
+
+    <!-- This is the body -->
+    <!-- This is where we place the content of our website -->
+    <body>
+        <header>
+            <h1> Manjit Gurcha </h1>
+        </header>
+        <nav>
+            <hr width="50%"/>
+            <a href="index.html"><span class="name"> Home </span></a>
+            <a href="about.html"> About </a>
+            <a href="contact.html"> Contact </a>
+        </nav>
+        
+        <main>
+            <figure id="me">
+                <img src = "img/my_img.jpg"/>
+            </figure>
+            <div id="welcomeText">
+                <br /><br />
+                Hello!<br />
+                <p>
+                   Thank you for visiting my professional portfolio website. 
+                </p>
+            
+                <p>
+                   I am currently a junior at CSU Monterey Bay majoring in computer science. 
+                   I transferred here from Sierra College after earning my Associates of Science 
+                   degree in computer science. 
+                </p>
+                    
+                <p>
+                   I'm looking for a internship opportunity. Feel free to contact me regarding any questions or concerns. 
+                </p>
+                <p>
+                   Thank you! 
+                </p>
+                
+                <br /><br />
+                <em>
+                    "With ordinary talent and extraordinary <strong>perserverance</strong>, all things are attainable"
+                </em><br />
+                - Thomas F. Buxton
+            </div>
+        </main>
+        
+        
+        <!-- This is the footer -->
+        <!-- The footer goes inside the body but not always -->
+        <footer>
+            <hr>
+            CST336. 2018 &copy; Gurcha <br />
+            <figure id = "foot">
+                 <img src = "img/csumb.jpg"/>
+            </figure>
+            <!--<strong>Disclaimer:</strong> The information in this webpage is true.<br />-->
+        </footer>
+        <!-- closing footer -->
+        
+    </body>
+    <!-- closing body -->
+
+</html>
